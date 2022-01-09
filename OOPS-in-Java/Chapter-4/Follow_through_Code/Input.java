84ed905b3f91b353c6ddbeb5a11f4d0ac0a7eba3
Darknez07 <rishikakkar42@gmail.com>
2022-01-09 06:36:04 +0530

    Chapter 4 folder

---
+++ OOPS-in-Java/Chapter-4/Follow_through_Code/Input.java
@@ -0,0 +1,37 @@
+package Follow_through_Code;
+import java.io.*;
+
+class CalculatorInput{
+    BufferedReader stream;
+    CalculatorEngine engine;
+    CalculatorInput(CalculatorEngine e){
+        InputStreamReader input = new InputStreamReader(System.in);
+        stream = new BufferedReader(input);
+        engine = e;
+    }
+    public void run() throws Exception{
+        for(;;){
+            System.out.print("["+engine.display()+"]");
+            String s = stream.readLine();
+            if(s == null) break;
+            if(s.length() > 0){
+                char c = s.charAt(0);
+                if(c == '+')  engine.add();
+                else if(c == '-') engine.subtract();
+                else if(c == '*') engine.multiply();
+                else if(c == '/') engine.divide();
+                else if(c >= '0' && c <= '9') engine.digit(c - '0');
+                else if(c == '=') engine.compute();
+                else if(c == 'c' || c == 'C') engine.clear();
+                else if(c == 'e' || c == 'E') break;
+            }
+        }
+    }
+};
+public class Input{
+    public static void main(String[] args) throws Exception{
+        CalculatorEngine en = new CalculatorEngine();
+        CalculatorInput ci = new CalculatorInput(en);
+        ci.run();
+    }
+};
