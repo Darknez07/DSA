75a5d5d64ed12d5222a2cbac4e055074b1f8b176
Darknez07 <rishikakkar42@gmail.com>
2022-01-14 01:18:25 +0530

    Stuff

--- OOPS-in-Java/Chapter-4/Follow_through_Code/Calculator.java
+++ OOPS-in-Java/Chapter-4/Follow_through_Code/Calculator.java
@@ -1,6 +1,7 @@
 class CalculatorEngine{
     int value,keep;
     char toDo;
+    boolean done;
     CalculatorEngine(){clear();}
     // Q1 and Q2 factorial is below
     public int fact(int n){return n <= 1 ? 1:n*fact(n - 1); }
@@ -21,10 +22,12 @@ class CalculatorEngine{
         else if(toDo == '/')
             value = keep/value;
         keep = 0;
+        done = true;
     }
     public void clear(){
         value = 0;
         keep = 0;
+        done = false;
     }
     public void binaryOperation(char op){
         keep = value;
@@ -32,6 +35,10 @@ class CalculatorEngine{
         toDo = op;
     }
     public void digit(int x){
+        if(done){
+            value = 0;
+            done  = false;
+        }
         if(x > 9 || x < 0){
             return;
         }else{
